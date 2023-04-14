f03b1cef6d1a26d43223dbca9794e06093e53c07
Willyan <wkinhaamaral@gmail.com>
2023-04-14 18:02:27 -0300

    versão sem bonus ok

--- script.js
+++ script.js
@@ -1,98 +1,121 @@
 axios.defaults.headers.common['Authorization'] = 'hrCVia1hqaWes5Q2Nv7VAikS';
-let sendname;
+let sendname, nomevalue,intermanter, intergetmsg;
 let msgserver = [];
+let endboxmsg = 0;
+
 
 function entrar() {
-
-
-
-    const nome = document.querySelector('.entername').value;
-    console.log(nome);
-
-    sendname = { name: nome };
-    console.log(sendname);
+    nomevalue = document.querySelector('.entername').value;
+    sendname = { name: nomevalue };
 
     const entrar = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', sendname);
     console.log(entrar);
     entrar.then(entradaok);
     entrar.catch(negado);
-
 }
+
 function manter() {
-    
-
     const status = axios.post('https://mock-api.driven.com.br/api/vm/uol/status', sendname);
-    status.catch(negado);
-    status.then(manterstatus())
-
-
+    status.catch(erroconect);
+    status.then(manterstatus);
 }
 
 function entradaok() {
     const batepapo = document.querySelector('.entrar');
     batepapo.classList.add('escondido')
     console.log("entradaok");
-    const inter = setInterval(manter, 4000);
+    intermanter = setInterval(manter, 5000);
+
+    
+    intergetmsg = setInterval(getmsg, 3000);
     getmsg();
 }
 
-function getmsg(){
+function getmsg() {
     console.log('get msg')
-    
+
     const msg = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages');
     msg.then(rendermsg);
     
-
-
 }
 
-function rendermsg(msg){ 
+function rendermsg(msg) {
     msgserver = msg.data;
     const ulmsg = document.querySelector('.mensagens');
     ulmsg.innerHTML = '';
+    for (let i = 0; i < msgserver.length; i++) {
 
-    // percorrer a minha lista de receitas
-    for( let i = 0; i < msgserver.length; i++){
-        // pergar receita por receita
         let msg = msgserver[i];
-        
-        if(msgserver[i].type==="status"){
-        ulmsg.innerHTML += `
+
+        if (msgserver[i].type === "status") {
+            ulmsg.innerHTML += `
             <li class='status'>
-            <h3><p class='time'>${msg.time}&nbsp; </p> <p class='from'> ${msg.from}&nbsp;</p><p class='txt'>${msg.text}&nbsp;</p></h3>                
+                <h3><p>
+                    <span class='time'>${msg.time}&nbsp;</span>
+                    <span class='from'> ${msg.from}&nbsp;</span>
+                    <span class='txt'>${msg.text}&nbsp;</span></p>
+                </h3>                
             </li>
-        `;}else{
+                `;
+        } else {
             ulmsg.innerHTML += `
             <li class='msgchat'>
-            <h3><p class='time'>${msg.time} </p> <p class='from'> ${msg.from} para ${msg.to}</p><p class='txt'>${msg.text}</p></h3>                
+                <h3><p>
+                    <span class='time'>${msg.time}&nbsp;</span>
+                    <span class='from'> ${msg.from}&nbsp;</span>
+                    <span>para&nbsp;</span>
+                    <span class='to'> ${msg.to}&nbsp;</span>
+                    <span class='txt'>${msg.text}</span></p>
+                </h3>                
             </li>
-        `;
-
+            `;
         }
+    }
+    if (endboxmsg === 0) {
+        const element = document.getElementById("Box");
+        element.scrollIntoView({ block: "end" });
     }
 }
 
 function negado(erro) {
     alert('ja existe usuário com este nome. Tente outro nome de usuário')
     console.log("negado")
-    console.log(erro)
-
 }
-function manterstatus(){
+
+function manterstatus() {
     console.log("conectado");
 }
 
-
-
-
-
-
-
-
-
-
 function sendmsg() {
-    const send = 1;
+    const msg = document.querySelector('.caixa').value;
+    if(msg!=""){
+    sendmsg1 = 
+    {
+        from: nomevalue,
+        to: "Todos",
+        text: msg,
+        type: "message"
+    };
+   
+    const waitsendmsg = axios.post('https://mock-api.driven.com.br/api/vm/uol/messages', sendmsg1);
+    waitsendmsg.then(msgenviada);
+    waitsendmsg.catch(erroconect);}
+}
+
+function msgenviada() {
+    getmsg();
+    const clean = document.querySelector('.caixa');
+    clean.value = '';
+}
+
+function erroconect() {
+    const batepapo = document.querySelector('.entrar');
+    batepapo.classList.remove('escondido');
+    endboxmsg = 0;
+    clearInterval(intergetmsg);
+    clearInterval(intermanter);
+    const ulmsg = document.querySelector('.mensagens');
+    ulmsg.innerHTML = '';
 
 }
 
